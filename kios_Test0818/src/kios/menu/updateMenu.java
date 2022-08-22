c7109b25ff98c22d04338741c5610c2886f13a29
JadenFM <111298803+JadenFM@users.noreply.github.com>
2022-08-22 17:47:49 +0900

    Kkr+phw 0822 (#29)
    * 0822_1736 경령+현우/장바구니 +,-,x버튼 반영 전
    
    * 0822_1738 경령+현우/장바구니 +,-,x버튼 반영 전
    
    * Update updateMenu.java
    
    Co-authored-by: DuBori <92709439+DuBori@users.noreply.github.com>

--- kios_Test0818/src/kios/menu/updateMenu.java
+++ kios_Test0818/src/kios/menu/updateMenu.java
@@ -1,5 +1,5 @@
 package kios.menu;
-
+ 
 import kios.db.DBconnection;
 import kios.db.Static;
 
@@ -38,6 +38,28 @@ public class updateMenu {
 			pstmt.setInt(7, shot);
 			pstmt.setInt(8, count);
 			pstmt.setInt(9, cost);
+			pstmt.executeUpdate();
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public updateMenu(String text,int cost,int count) {
+		try {
+			con= DBconnection.getConnection();
+			find=findId("select product_id from product where product_name=?",text);
+			
+			query="insert into menu_product values(?,?,?,?,?,?,?,?,?)";
+			pstmt=con.prepareStatement(query);
+			pstmt.setInt(1, Static.count); // BILL_ID
+			pstmt.setInt(2, find); // PRODUCT_ID
+			pstmt.setString(3, text); // PRODUCT_NAME
+			pstmt.setString(4, null); // BILL_SIZE
+			pstmt.setString(5, null); // BILL_TEMPER
+			pstmt.setInt(6, 0); // BILL_DEFAULTSIZE = 커피 단가+사이즈 가격
+			pstmt.setInt(7, 0); // BILL_SHOT
+			pstmt.setInt(8, count); // BILL_COUNT
+			pstmt.setInt(9, cost); // BILL_COST
 			pstmt.executeUpdate();
 		
 		} catch (Exception e) {
